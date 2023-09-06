60034e91d4091b0bdeaf7f612358ba568bd35f70
JustinLafay <justin.lafay@insa-lyon.fr>
2023-09-06 01:12:27 -0300

    modif eje1 add eje2

--- gpio_configurado_como_salida_2/eje2Lafay.c
+++ gpio_configurado_como_salida_2/eje2Lafay.c
@@ -1,7 +1,7 @@
 /*
  * Estudiante: Justin Lafay
  * Carrera: Electronica
- * Descripci�n: 
+ * Descripci�n:
  *
  */
 
@@ -12,19 +12,18 @@ void retardo(uint32_t contador);
 int main(void){
 
 	uint32_t relojCpu = SystemCoreClock;
+	int numDisplay [10] ={0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67};
 
 	//LPC_PINCON->PINSEL1 &= ~(3<<12);   // 0B00111111111111 & 0B10010101010101 = 0B00_010101010101
-	LPC_GPIO0->FIODIR     |= (0b1111111111<<0);     // 0B1000...000 | 0B010101..101 = 1_10101..101
+	LPC_GPIO2->FIODIR0     |= (0b11111111<<0);     // 0B1000...000 | 0B010101..101 = 1_10101..101
 	//LPC_GPIO0->FIOMASK =0xFFFFFFFF;  //~FIOMASK & REGISTRO =0000
 
-	int seq1 = 0b1100100101;
-	int seq2 = 0b0011100011;
-
 	while(1){
-		LPC_GPIO0->FIOCLR |= (seq1<<0);
-		retardo(relojCpu);
-		LPC_GPIO0->FIOSET |= (seq2<<0);
-		retardo(relojCpu);
+		for (int i=0; i<10; i++){
+			LPC_GPIO2->FIOSET0 &= (numDisplay[i]);
+			LPC_GPIO2->FIOSET0 |= (numDisplay[i]);
+			retardo(relojCpu);
+		}
 	}
 
 	return 0;
