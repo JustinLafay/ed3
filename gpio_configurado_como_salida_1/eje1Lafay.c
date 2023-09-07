7f9f1ff7bf07d6a4a34015290de924744c5beb0e
JustinLafay <justin.lafay@insa-lyon.fr>
2023-09-07 02:29:19 -0300

    change eje1

--- gpio_configurado_como_salida_1/eje1Lafay.c
+++ gpio_configurado_como_salida_1/eje1Lafay.c
@@ -1,7 +1,6 @@
 /*
  * Estudiante: Justin Lafay
  * Carrera: Electronica
- * Descripci�n: 
  *
  */
 
@@ -13,18 +12,16 @@ int main(void){
 
 	uint32_t relojCpu = SystemCoreClock;
 
-	//LPC_PINCON->PINSEL1 &= ~(3<<12);   // 0B00111111111111 & 0B10010101010101 = 0B00_010101010101
-	LPC_GPIO0->FIODIR     |= (0b1111111111<<0);     // 0B1000...000 | 0B010101..101 = 1_10101..101
-	//LPC_GPIO0->FIOMASK =0xFFFFFFFF;  //~FIOMASK & REGISTRO =0000
+	LPC_GPIO0->FIODIR |= (0b1111111111<<0); // select 9 first pin as out (1)
 
-	int seq1 = 0b1100100101;
-	int seq2 = 0b0011100011;
+	int seq [1] = {0b1100100101, 0b001110001}; // table of 2 sequences of 10 LED
 
 	while(1){
-		LPC_GPIO0->FIOCLR |= (seq1<<0);
-		retardo(relojCpu);
-		LPC_GPIO0->FIOSET |= (seq2<<0);
-		retardo(relojCpu);
+		for (int i = 0; i<2; i++){
+			LPC_GPIO0->FIOSET &= (seq[i]<<0);
+			LPC_GPIO0->FIOSET |= (seq[i]<<0);
+			retardo(relojCpu);
+		}
 	}
 
 	return 0;
