4191afebfcfe251c5739ccd0eb81445aa44a4c46
JustinLafay <justin.lafay@insa-lyon.fr>
2023-09-09 03:11:53 -0300

    add ejecourso eje5

--- gpio_configurado_como_entrada_3/eje5Lafay.c
+++ gpio_configurado_como_entrada_3/eje5Lafay.c
@@ -9,12 +9,12 @@
 int main(void){
 	short buffer[8];
 	short result = 0;
-	short pin_out_l;
-	short pin_out_h;
+	short pin_out_l = 0;
+	short pin_out_h = 0;
 	int point = 0;
 
-	LPC_GPIO1->FIODIRL &= ~(0b1111111111111111<<0); // input mode from P1.0 to P1.15
-	LPC_GPIO0->FIODIR0 |= (0b1111000111111111111<<0); // output mode from
+	LPC_GPIO1->FIODIR &= ~(0b1111111111111111<<0); // input mode from P1.0 to P1.15
+	LPC_GPIO0->FIODIR |= (0b1111000111111111111<<0); // output mode
 
 	while(1){
 		result = 0;
@@ -26,9 +26,11 @@ int main(void){
 		for (int i; i<7; i++){
 			result = result+(buffer[i]); // calculate the buffer
 		}
-		 pin_out_h = result & (0b111<<15);
+
+		 pin_out_h = result & (0b1111<<15);
 		 pin_out_l = result & (0b111111111111<<0);
 		 result = pin_out_l | pin_out_h;
+
 		LPC_GPIO0->FIOSET0 &= result;
 		LPC_GPIO0->FIOSET0 |= result;
 
